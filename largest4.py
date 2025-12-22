c919dc889ddee07d88cd366c03e8e38339641b2d
Vinay U <uchihaitachi11329@gmail.com>
2025-12-22 19:06:18 +0530

    Add files via upload

---
+++ largest4.py
@@ -0,0 +1,15 @@
+a=int(input('enter the 1st number:'))
+b=int(input('enter the 2nd number:'))
+c=int(input('enter the 3rd number:'))
+d=int(input('enter the 4th number:'))
+if a>b:
+    if a>c:
+         if a>d:
+            print('a is greatest')
+         else:
+            print('d is greatest')
+else:
+    if b>c:
+        print('b is greatest')
+    else:
+        print('c is greatest')
